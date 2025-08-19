ffc3ebadc8782d24539b50baceb87981791be464
Vinicius <gontijoweb.dev@gmail.com>
2025-08-19 07:29:55 -0300

    animacoes

---
+++ Barbeiro/js/main.js
@@ -0,0 +1,28 @@
+const animDireita = document.querySelectorAll('.anim-direita')
+const animEsquerda = document.querySelectorAll('.anim-esquerda')
+
+window.addEventListener('scroll', checkAnim)
+
+checkAnim()
+
+function checkAnim () {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    animDireita.forEach(anim => {
+        const animTop = anim.getBoundingClientRect().top
+
+        if (animTop < triggerBottom) {
+            anim.classList.add('rolagem-direita')
+            anim.classList.remove('anim-direita')
+        }
+    })
+
+    animEsquerda.forEach(anim => {
+        const animTop = anim.getBoundingClientRect().top
+
+        if (animTop < triggerBottom) {
+            anim.classList.add('rolagem-esquerda')
+            anim.classList.remove('anim-esquerda')
+        }
+    })
+}
